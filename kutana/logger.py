08c6e3b561667217c1e557e8ddcc53b67b0050f3
Sergey Abroskin <41290484+MerdedSpade@users.noreply.github.com>
2018-12-10 22:10:49 +0300

    Update logger.py

--- kutana/logger.py
+++ kutana/logger.py
@@ -1,18 +1,4 @@
-import logging
+from loguru import logger
+logger.start("kutana-{time}.log", compression="zip", rotation="2 MB")
 
 
-formatter = logging.Formatter('%(asctime)s [ %(levelname)s ] %(message)s')
-level = logging.DEBUG
-
-handler_file = logging.FileHandler('kutana.log')
-handler_file.setLevel(level)
-handler_file.setFormatter(formatter)
-
-handler_stream = logging.StreamHandler()
-handler_stream.setLevel(level)
-handler_stream.setFormatter(formatter)
-
-logger = logging.getLogger("kutana")
-logger.setLevel(level)
-logger.addHandler(handler_stream)
-logger.addHandler(handler_file)

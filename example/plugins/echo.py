2c62c7a87f13769c0e64a88aeeb2d02bb24b344c
Michael Krukov <krukov.michael@ya.ru>
2020-08-06 01:48:50 +0300

    Minor tweak for echo command

--- example/plugins/echo.py
+++ example/plugins/echo.py
@@ -6,4 +6,4 @@ plugin = Plugin(name="Echo", description="Reply with send message")
 
 @plugin.on_commands(["echo"])
 async def _(msg, ctx):
-    await ctx.reply("{}".format(ctx.body), attachments=msg.attachments)
+    await ctx.reply("{}".format(ctx.body or '(/)'), attachments=msg.attachments)

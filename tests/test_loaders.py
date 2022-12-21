00a79674dd271e29bd7befb53bae95dffd3604b0
Aaron Chong <aaronjhchong@hotmail.com>
2022-12-21 21:25:14 +0800

    Add failing test_pickle_dumps

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -1,4 +1,7 @@
+import pickle
 from os.path import dirname
+from unittest.mock import patch
+
 from kutana.loaders import load_plugins
 
 
@@ -7,3 +10,13 @@ def test_load_plugins():
 
     assert len(plugins) == 3
     assert {"echo", "hello 1", "hello 2"} == set(p.name for p in plugins)
+
+
+@patch("kutana.plugin.Plugin.on_match")  # on_match is incompatible with pickle
+def test_pickle_dumps(_):
+    plugins = load_plugins("tests/assets", verbose=True)
+
+    assert len(plugins) == 3
+    assert {"echo", "hello 1", "hello 2"} == set(p.name for p in plugins)
+
+    pickle.dumps(plugins)
